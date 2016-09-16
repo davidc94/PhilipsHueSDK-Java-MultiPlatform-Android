3b4904d6a52dca439838693b9790a73d5a2ee8a9
davidc94 <davidc94@btinternet.com>
2016-09-16 22:01:35 +0100

    settings changes

---
+++ QuickStartApp/gen/com/philips/lighting/quickstart/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.philips.lighting.quickstart;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
